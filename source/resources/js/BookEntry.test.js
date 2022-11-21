de95096632fb5e76d7f0ce0210b17475370286eb
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-11-20 16:26:59 -0800

    fix Date

--- source/resources/js/BookEntry.test.js
+++ source/resources/js/BookEntry.test.js
@@ -9,7 +9,7 @@ const BookEntryClass = require('./placeholder.js')
 test('valid constructor', () => {
 
   //const testBookEntryForConstructor = new BookEntry(new Set(['Sci-fi']), new Date('November 17, 2022 03:24:00'), 9, 340, 'completed', 'I thought it was poignantly pointless.', 'A4238A53I1', 'Robert Heinlein', 'Stranger in a Strange Land')  ;  
-  const testBookEntryForConstructor = BookEntryClass.bookentry(new Set(['Sci-fi', 'Fiction']), new Date(Date.UTC('November 17, 2022 03:24:00')), 9, 340, 'completed', 'I thought it was poignantly pointless.', 'A4238A53I1', 'Robert Heinlein', 'Stranger in a Strange Land')  ;  
+  const testBookEntryForConstructor = BookEntryClass.bookentry(new Set(['Sci-fi', 'Fiction']), new Date(), 9, 340, 'completed', 'I thought it was poignantly pointless.', 'A4238A53I1', 'Robert Heinlein', 'Stranger in a Strange Land')  ;  
 
   expect(testBookEntryForConstructor.printBookEntry()).toBe('Stranger in a Strange Land by: Robert Heinlein, rating: 9, progress: 340, status: completed, Date read: Thu Nov 17 2022 03:24:00 GMT-0800 (Pacific Standard Time) \nReview: I thought it was poignantly pointless., ISBN: A4238A53I1, tags: Sci-fi, Fiction');
 }); 
