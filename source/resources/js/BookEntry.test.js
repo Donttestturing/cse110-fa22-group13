0b42595def51e904bcea55742e1ddbb0282c48a1
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-11-20 16:11:38 -0800

    add test

--- source/resources/js/BookEntry.test.js
+++ source/resources/js/BookEntry.test.js
@@ -4,7 +4,7 @@
 */
 const BookEntryClass = require('./placeholder.js')
 //import {BookEntryClass} from './BookEntry.js';
-/*
+
 //test constructor/setters
 test('valid constructor', () => {
 
@@ -12,7 +12,7 @@ test('valid constructor', () => {
   const testBookEntryForConstructor = BookEntryClass.bookentry(new Set(['Sci-fi', 'Fiction']), new Date('November 17, 2022 03:24:00'), 9, 340, 'completed', 'I thought it was poignantly pointless.', 'A4238A53I1', 'Robert Heinlein', 'Stranger in a Strange Land')  ;  
 
   expect(testBookEntryForConstructor.printBookEntry()).toBe('Stranger in a Strange Land by: Robert Heinlein, rating: 9, progress: 340, status: completed, Date read: Thu Nov 17 2022 03:24:00 GMT-0800 (Pacific Standard Time) \nReview: I thought it was poignantly pointless., ISBN: A4238A53I1, tags: Sci-fi, Fiction');
-}); */
+}); 
 
 test('empty string constructor', () => {
   const testEntryEmptyStrings = BookEntryClass.bookentry('', '', '', '', '', '', '', '', '');
