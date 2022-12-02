aaa1c1894143f040339d8f427a4ebb13645bc144
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-12-01 19:09:04 -0800

    Update bookentry.js

--- source/bookEntries/bookentry.js
+++ source/bookEntries/bookentry.js
@@ -5,8 +5,8 @@
 * 
 */
  const STATUSES = ['completed', 'in progress', 'planned'] 
+ 
 /**
-* 
 * @class bookeeeeeeeentry
 * The BookEntry class, which represents a book entry, which includes a book and the requisite information like title, ISBN, and authorName, as well as ancillary information like the status of being read, 
 * the current review for the book, the genres the book is tagged in, the date the book was read (if finished) and the current page progress of the user for that book. 
